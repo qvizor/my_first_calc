12e30a7d051dbe411f9e183ee9c662cd5af9da94
x-dev <aprogramus@gmail.com>
2016-05-21 16:10:08 +0300

    ещ 50

--- generator.js
+++ generator.js
@@ -5,12 +5,12 @@ code += 'var number1 = process.argv[2];\n';
 code += 'var operation = process.argv[3];\n';
 code += 'var number2 = process.argv[4];\n';
 
-for (var i = 0; i < 100; i++) {
-    for (var j = 0; j < 100; j++) {
+for (var i = 0; i < 50; i++) {
+    for (var j = 0; j < 50; j++) {
         code += 'if (number1 === "' + i + '" && operation === "+" && number2 === "' + j + '") console.log("' + (i + j) + '");\n';
         code += 'if (number1 === "' + i + '" && operation === "-" && number2 === "' + j + '") console.log("' + (i - j) + '");\n';
         code += 'if (number1 === "' + i + '" && operation === "*" && number2 === "' + j + '") console.log("' + (i * j) + '");\n';
     }
 }
 
-fs.writeFileSync('calc100.js', code);
+fs.writeFileSync('calc50.js', code);
