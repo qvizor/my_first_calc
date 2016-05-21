9575cd66b71fe9ef64c91772dcfb6ba97ec3bf1d
x-dev <aprogramus@gmail.com>
2016-05-21 16:07:51 +0300

    git cant view big files

--- generator.js
+++ generator.js
@@ -5,12 +5,12 @@ code += 'var number1 = process.argv[2];\n';
 code += 'var operation = process.argv[3];\n';
 code += 'var number2 = process.argv[4];\n';
 
-for (var i = 0; i < 200; i++) {
-    for (var j = 0; j < 200; j++) {
+for (var i = 0; i < 100; i++) {
+    for (var j = 0; j < 100; j++) {
         code += 'if (number1 === "' + i + '" && operation === "+" && number2 === "' + j + '") console.log("' + (i + j) + '");\n';
         code += 'if (number1 === "' + i + '" && operation === "-" && number2 === "' + j + '") console.log("' + (i - j) + '");\n';
         code += 'if (number1 === "' + i + '" && operation === "*" && number2 === "' + j + '") console.log("' + (i * j) + '");\n';
     }
 }
 
-fs.writeFileSync('calc.js', code);
+fs.writeFileSync('calc100.js', code);
